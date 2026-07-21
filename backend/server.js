fb13e0e7318a9c552741146e61a25d78f8be4520
Dhanu-06 <u25uv23t029021@gmail.com>
2026-07-21 18:15:31 +0530

    Allow Vercel frontend

--- backend/server.js
+++ backend/server.js
@@ -7,11 +7,11 @@ connectDB();
 
 const app = express();
 
-const cors = require('cors');
+//const cors = require('cors');
 
 app.use(cors());
 app.use(express.json());
-//app.use(express.json());
+
 
 // Routes
 app.use('/api/auth', require('./routes/auth'));
