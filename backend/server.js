50ff5593c437b6f5f4b276e14c53cf1c0041f650
Dhanu-06 <u25uv23t029021@gmail.com>
2026-07-21 14:07:17 +0530

    Update CORS for Vercel frontend

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,15 @@ connectDB();
 
 const app = express();
 
-app.use(cors({ origin: ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:5175'], credentials: true }));
+app.use(cors({
+  origin: [
+    'http://localhost:5173',
+    'http://localhost:5174',
+    'http://localhost:5175',
+    'https://medora-kot9.vercel.app'
+  ],
+  credentials: true
+}));
 app.use(express.json());
 
 // Routes
